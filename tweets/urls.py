ebce2122b5e32b625d49f3efa4d31b0e913dfaf7
Sourabh Kulkarni <sourabhk1998@gmail.com>
2020-07-19 19:54:51 +0530

    Talkbits with tests,( before React)

--- tweets/urls.py
+++ tweets/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,re_path,include
-from tweets.views import home_view,tweet_detail_view,tweet_list_view,tweet_create_view, tweet_delete_view,tweet_action_view
+from django.urls import path
+from .views import home_view,tweet_detail_view,tweet_list_view,tweet_create_view, tweet_delete_view,tweet_action_view
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',home_view ),
-    path('tweets/<int:tweet_id>',tweet_detail_view),
-    path('tweets/',tweet_list_view),
-    path('create-tweet',tweet_create_view),
-    #path('api/tweets/<int:tweet_id>/delete',tweet_delete_view),
-    #path('api/tweets/action/',tweet_action_view),
-    path('api/tweets/',include('tweets.urls'))
+    path('',tweet_list_view),
+    path('action/',tweet_action_view),
+    path('create/',tweet_create_view),
+    path('<int:tweet_id>/',tweet_detail_view),
+    path('<int:tweet_id>/delete/',tweet_delete_view),
+    
+    
+    
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -70,7 +70,8 @@ def tweet_action_view( request, *args, **kwargs):
 
         elif action=="unlike":
             obj.likes.remove(request.user)
-
+            serializer=TweetSerializer(obj)
+            return Response(serializer.data,status=200)
         elif action=="retweet":
             
             new_tweet=Tweet.objects.create(
@@ -79,7 +80,7 @@ def tweet_action_view( request, *args, **kwargs):
                 content=content
             )
             serializer=TweetSerializer(new_tweet)
-            return Response(serializer.data,status=200)
+            return Response(serializer.data,status=201)
             
 
     return Response({},status=200)
