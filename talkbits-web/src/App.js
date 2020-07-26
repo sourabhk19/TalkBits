a5cd5d509e0c2677c6f05f3489fdc0a216b8bef4
Sourabh Kulkarni <sourabhk1998@gmail.com>
2020-07-26 15:21:26 +0530

    Functional Components in React JS

--- talkbits-web/src/App.js
+++ talkbits-web/src/App.js
@@ -2,12 +2,50 @@ import React,{useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function loadTweets(callback){
+  const xhr = new XMLHttpRequest()
+  const method = 'GET' // "POST"
+  const url = "http://localhost:8000/api/tweets/"
+  const responseType = "json"
+  xhr.responseType = responseType
+  xhr.open(method, url)
+  xhr.onload = function() {
+    callback(xhr.response, xhr.status)
+}
+xhr.onerror=function(e){
+  console.log(e)
+  callback({"message":"The request was an error"},400)
+}
+xhr.send()
+}
+
+function Tweet(props) {
+  const {tweet} = props
+  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+  return <div className={className}>
+      <p>{tweet.id} - {tweet.content}</p>
+  </div>
+}
+
+
 function App() {
   const [tweets,setTweets]=useState([{content: 143}])
+  
   useEffect(()=>{
+    const myCallback=(response,status)=>{
+      console.log(response,status)
+      //const TweetItems=[{"content" : 123},{"content" :" yo yo"}]
+      if(status===200){
+      setTweets(response)
+      }
+      else{
+        alert("There was an error")
+      }
+    }
+    loadTweets(myCallback)
     //do lookup
-    const TweetItems=[{"content" : 123},{"content" :" yo yo"}]
-    setTweets(TweetItems)
+   
   },[])
   return (
     <div className="App">
@@ -16,11 +54,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          {tweets.map((tweet,index)=>{
-            return <li>{tweet.content}</li>
+        <div>
+          {tweets.map((item, index)=>{
+            return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`} />
           })}
-        </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
--- talkbits/settings.py
+++ talkbits/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#1!mc%r88r_8ne9c)p_5w$i0c0%tsll!=e$k4-2g1a^4u5b-8l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 MAX_TWEET_LENGTH = 240
 TWEET_ACTION_OPTIONS=["like","unlike","retweet"]
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #third party
+    'corsheaders',
     'rest_framework',
     #internal
     'tweets',
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -124,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_URLS_REGEX = r'^/api/.*$'
 
 DEFAULT_RENDERER_CLASSES = [
         'rest_framework.renderers.JSONRenderer',
