7cdaf487ede0422af79b33ce278cb0963f3bfe64
Sourabh Kulkarni <sourabhk1998@gmail.com>
2020-08-02 22:24:51 +0530

    Build and Using on Django,with static file loading error

--- tweets/views.py
+++ tweets/views.py
@@ -21,6 +21,17 @@ def home_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hello World</h1>")
     return render(request,"pages/home.html",context={})
 
+def local_tweets_list_view(request, *args, **kwargs):
+    return render(request, "tweets/list.html")
+
+def local_tweets_detail_view(request, tweet_id, *args, **kwargs):
+    return render(request, "tweets/detail.html", context={"tweet_id": tweet_id})
+
+def local_tweets_profile_view(request, username, *args, **kwargs):
+    return render(request, "tweets/profile.html", context={"profile_username": username})
+
+
+
 @api_view(['GET'])
 def tweet_detail_view( request,tweet_id, *args, **kwargs):
     qs=Tweet.objects.filter(id=tweet_id)
